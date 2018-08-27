5066b935d8fe117a88ff61207cdddd13d1071bec
José Nieto <jdnv.123@gmail.com>
2018-08-27 00:06:59 -0500

    Click on icon match

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <ctime>
 #include <csignal>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <stdlib.h>
 #include <errno.h>
-
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
@@ -26,7 +26,7 @@ const double DLNG   = 0.0206;  // x-axis, decrease left
 const double DLOAD  = 5;      // console progress bar
 const char* IMGNAME = "screenshot.png";
 char *DIR = "icons/";
-char* image_window = "Source Image";
+char *image_window = "Source Image";
 
 struct Location {
   double lat;
@@ -61,76 +61,69 @@ double THRESHOLDS[NMODS][2] = {
 };
 
 int gidx = 0;
-struct Location grid[Q];  // FIXME: bug - can't declare grid[] before READFILES[]! 
+struct Location map[Q];  // FIXME: bug - can't declare map[] before READFILES[]! 
 
+void setup(int argc, char *argv[]);
 void setupFlags(int argc, char *argv[]);
 
-string currentDateTime();
-bool doesMatch(float f);
-void chksyscall(char* line);
 void clean();
-void fetchMatches(char* imgname, char* templname, void *out, double threshold);
-void fillCol(int init, double iniLat, double iniLng, int n);
-void getCoordinates(int pixelLng, int pixelLat, double lat, double lng, void *res);
-void fillGrid();
+void chksyscall(string line);
 void signalHandler( int signum );
+
+string currentDateTime();
+int printProgressBar(int i, int load);
+void getMapScreenshot(int i, double lat, double lng);
+void generateMap();
+void fillMapCol(int init, double iniLat, double iniLng, int n);
+
+bool matches(float f);
+void fetchPositions(char* imgname, char* templname, void *out, double threshold);
+void getCoordinates(int pixelLng, int pixelLat, double lat, double lng, void *res);
 void writeMatch(char* templname, char* label, double lat, double lng, double threshold);
 void writeMatches(double lat, double lng);
 
+// ────────────────────────────────────────────────────────────────────────────────
+
+
 int main (int argc, char *argv[]) {
-  setupFlags(argc, argv);
-  // chksyscall("./setup.sh");
-  signal(SIGINT, signalHandler);
-  bash.precision(PRECISION);
-  data.precision(PRECISION);
-  cout.precision(PRECISION);
+  cout << "WazeReader\tScan traffic events through Bogota, Colombia\n";
+  cout << "\t\tBeginning execution at " << currentDateTime() << "\n\n";
 
-  cout << "\n\nBeginning execution at " << currentDateTime() << "\n\n";
-  cout << "WAZEREAD\tScan traffic events through Bogota, Colombia\n\t\t";
-
-  fillGrid();
+  setup(argc, argv);
+  generateMap(); // FIXME: some coordinates are off place at first iteration
 
   int load = 0;   // used in console progress bar
   for(int i = 0; i < Q; i++) {
-    // print progress bar
-    if(((i+1)*100/Q) >= load) {
-      cout << load << "% " << (load/10 == 0 ? " " : "") << "[";
-      for(int j = 0; j < load; j+=DLOAD) cout << "==";
-      for(int j = load; j < 100; j+=DLOAD)  cout << "  ";
-      cout << "]" << endl;
-      load+=DLOAD;
-    }
+    load = printProgressBar(i, load);
 
-    double lat = grid[i].lat;
-    double lng = grid[i].lng;
+    double lat = map[i].lat;
+    double lng = map[i].lng;
 
-    bash.open ("script.sh");
-    bash << "#!/bin/bash\n"
-      << "google-chrome "
-      << " \'https://www.waze.com/es-419/livemap?zoom=" << ZOOM
-      << "&lat=" << lat << "&lon=" << lng << "\' &> /dev/null\n"
-      << (i == 0 ? "xdotool key F11\n" : "")
-      << "sleep 5\n"
-      << "scrot " << IMGNAME << "\n"
-      << "xdotool key Ctrl+w\n";
-    
-    bash.close();
-
-    chksyscall( (char*)"chmod +x script.sh" );
-    chksyscall( (char*)"./script.sh" );
-
+    getMapScreenshot(i, lat, lng);
     writeMatches(lat, lng);
+
+    // close Chrome tab
+    chksyscall("xdotool key Ctrl+w");
   }
 
-  cout << "\nfinished scanning at " << currentDateTime() << endl;
-  cout << "map reading done. fetching coordinates from file...\n";
-
+  cout << "\Map scanning concluded at " << currentDateTime() << "\n";
   clean();
   return 0;
 }
 
+void setup(int argc, char *argv[]) {
+  setupFlags(argc, argv);
+  // chksyscall("./setup.sh");
+  signal(SIGINT, signalHandler);
+
+  bash.precision(PRECISION);
+  data.precision(PRECISION);
+  cout.precision(PRECISION);
+}
+
 void setupFlags(int argc, char *argv[]) {
   bool opt = false;
+  cout << "\t\t";
   if(argc > 1) {
     for(int i = 1; i < argc; i++) {
       graphicMode = graphicMode || strcmp(argv[i], (char *)"--graphic") == 0;
@@ -160,14 +153,39 @@ void setupFlags(int argc, char *argv[]) {
         READFILES[i] = true;
     }
   } else {
-    cout << "DEFAULT MODE: will scan everything!\n";
+    cout << "DEFAULT mode -- will scan everything!\n";
     for(int i=0; i<NMODS; i++)
       READFILES[i] = true;
   }
 }
 
+int printProgressBar(int i, int load) {
+  if(((i+1)*100/Q) >= load) {
+    cout << load << "% " << (load/10 == 0 ? " " : "") << "[";
+    for(int j = 0; j < load; j+=DLOAD) cout << "==";
+    for(int j = load; j < 100; j+=DLOAD)  cout << "  ";
+    cout << "]" << endl;
+    load+=DLOAD;
+  }
+  return load;
+}
+
+void getMapScreenshot(int i, double lat, double lng) {
+  bash.open ("script.sh");
+  bash << "#!/bin/bash\n"
+    << "google-chrome "
+    << " \'https://www.waze.com/es-419/livemap?zoom=" << ZOOM
+    << "&lat=" << lat << "&lon=" << lng << "\' &> /dev/null\n"
+    << (i == 0 ? "xdotool key F11\n" : "")
+    << "sleep 3\n"  // TODO: make sleep a variable
+    << "scrot " << IMGNAME << "\n";
+  bash.close();
+  chksyscall("chmod +x script.sh");
+  chksyscall("./script.sh");
+}
+
 void writeMatches(double lat, double lng) {
-  for(int i=0; i<NMODS; i++) {
+  for(int i=4; i<NMODS; i++) {
     if(READFILES[i]) {
       char filename[50];
       strcpy(filename, DIR);
@@ -181,7 +199,7 @@ void writeMatch(char *templname, char* label, double lat, double lng, double thr
   struct Location loc;
   vector<Point> points;
 
-  fetchMatches( (char*) IMGNAME, templname, &points, threshold );
+  fetchPositions( (char*) IMGNAME, templname, &points, threshold );
 
   if(points.size() >= 1) {
     char filename[50];
@@ -189,6 +207,14 @@ void writeMatch(char *templname, char* label, double lat, double lng, double thr
     strcat(filename, "-wr.log");
     data.open(filename, ios::app);
     for(vector<Point>::const_iterator pos = points.begin(); pos != points.end(); ++pos) {
+      double posX = pos->x+ICON/2;
+      double posY = pos->y+ICON/2;
+
+      stringstream click;
+      click << "xdotool mousemove " << posX << " " << posY << " click 1";      
+      chksyscall(click.str());
+      // TODO: read commentary above
+
       getCoordinates(pos->x+ICON/2, pos->y+ICON/2, lat, lng, &loc);
       if(!silentMode) cout << "*** found " << label << " at " << lat << ", " << lng << "\n";
       data << currentDateTime() << "," << loc.lat << "," << loc.lng << endl;
@@ -211,8 +237,8 @@ void getCoordinates(int pixelLng, int pixelLat, double lat, double lng, void *re
   locRes->lng = resLng;
 }
 
-void chksyscall(char* line) {
-  int status = system(line);
+void chksyscall(string line) {
+  int status = system(line.c_str());
   if (status < 0) {
     cout << "Error: " << strerror(errno) << '\n';
     clean();
@@ -228,28 +254,28 @@ void chksyscall(char* line) {
   }
 }
 
-void fillCol(double iniLat, double iniLng, int n) {
-  grid[gidx].lat = iniLat;
-  grid[gidx].lng = iniLng;
+void fillMapCol(double iniLat, double iniLng, int n) {
+  map[gidx].lat = iniLat;
+  map[gidx].lng = iniLng;
 
   for(int i = 0; i < n; i++) {
-    grid[++gidx].lat = grid[gidx-1].lat - DLAT;
-    grid[gidx].lng = iniLng;
+    map[++gidx].lat = map[gidx-1].lat - DLAT;
+    map[gidx].lng = iniLng;
   }
 }
 
-void fillGrid() {
+void generateMap() {
   gidx = 0;
-  fillCol(4.8196, -74.02588, 11);
-  fillCol(4.7980, -74.04648, 13);
-  fillCol(4.7635, -74.06708, 18);
-  fillCol(4.7569, -74.08768, 23);
-  fillCol(4.7569, -74.10828, 25);
-  fillCol(4.7421, -74.12888, 19);
-  fillCol(4.6989, -74.14948, 15);
-  fillCol(4.6532, -74.17008, 8);
-  fillCol(4.6424, -74.19068, 6);
-  fillCol(4.6316, -74.21128, 5);
+  fillMapCol(4.8196, -74.02588, 11);
+  fillMapCol(4.7980, -74.04648, 13);
+  fillMapCol(4.7635, -74.06708, 18);
+  fillMapCol(4.7569, -74.08768, 23);
+  fillMapCol(4.7569, -74.10828, 25);
+  fillMapCol(4.7421, -74.12888, 19);
+  fillMapCol(4.6989, -74.14948, 15);
+  fillMapCol(4.6532, -74.17008, 8);
+  fillMapCol(4.6424, -74.19068, 6);
+  fillMapCol(4.6316, -74.21128, 5);
 }
 
 string currentDateTime() {
@@ -279,7 +305,7 @@ void clean() {
   cout << "done." << endl;
 }
 
-bool doesMatch(float f, double threshold) {
+bool matches(float f, double threshold) {
   if( match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED ) {
     return (f <= threshold);
   } else {
@@ -287,7 +313,7 @@ bool doesMatch(float f, double threshold) {
   }
 }
 
-void fetchMatches( char* imgname, char* templname, void *out, double threshold ) {
+void fetchPositions( char* imgname, char* templname, void *out, double threshold ) {
   /// Load image and template
   Mat img_display;
 
@@ -320,7 +346,7 @@ void fetchMatches( char* imgname, char* templname, void *out, double threshold )
     for(int j = 0; j < result_cols; j++) {
       float res = result.at<float>(i,j);
       if(res > max) max = res;
-      if(doesMatch(res, threshold)) {
+      if(matches(res, threshold)) {
         if(graphicMode || debugMode) {
           found = true;
           cout << "(" << j << ", " << i << "): " << res << endl;
